603a166a0f112f3679538f027f73fc1ad2ee01fa
Adil Minocherhomjee <adilminocherhomjee@gmail.com>
2020-04-21 16:25:49 -0600

    added colors, implemented the min/max and luminosity brightness mappings, added the option to invert all the brightnesses

--- ascii-art.js
+++ ascii-art.js
@@ -1,8 +1,10 @@
 const Caman = require('caman').Caman;
+var colors = require('colors/safe');
+
 //Note: With error "TypeError: Canvas is not a constructor", update node_modules/caman/dist/caman.full.js on line 371 to this.canvas = new Canvas.Canvas(this.imageWidth(), this.imageHeight()); AND on line 2530, update canvas = new Canvas.Canvas(newDims.width, newDims.height);
 
 const picture = Caman('img/ascii-pineapple.jpg', function () {
-  const factor = 7;
+  const factor = 3;
   this.resize({
     width: this.width / factor,
     height: this.height / factor,
@@ -13,9 +15,23 @@ const picture = Caman('img/ascii-pineapple.jpg', function () {
   const brightness =
     '`^",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$';
   for (let i = 0; i < this.pixelData.length; i += 4) {
-    const [red, green, blue, alpha] = this.pixelData.slice(i, i + 4);
+    let [red, green, blue, alpha] = this.pixelData.slice(i, i + 4);
+    const inverse = true;
+    if (inverse) {
+      red = 255 - red;
+      green = 255 - green;
+      blue = 255 - blue;
+      alpha = 255 - alpha;
+    }
     const average = Math.round((((red + green + blue) / 3) * 65) / 255);
-    rgbaArray.push({ average, red, green, blue, alpha });
+    const lightness = Math.round(
+      (((Math.max(red, green, blue) + Math.min(red, green, blue)) / 2) * 65) /
+        255
+    );
+    const luminosity = Math.round(
+      (0.21 * red + 0.72 * green + 0.07 * blue) / 255
+    );
+    rgbaArray.push({ average, lightness, luminosity, red, green, blue, alpha });
   }
 
   const twoDimensions = [];
@@ -23,13 +39,11 @@ const picture = Caman('img/ascii-pineapple.jpg', function () {
     const row = rgbaArray.slice(i, i + this.width);
     twoDimensions.push(row);
   }
-  const final = twoDimensions.map((element) =>
-    element
-      .map((rgba) =>
-        [brightness[rgba.average], brightness[rgba.average]].join('')
-      )
-      .join('')
-  );
+  const final = twoDimensions
+    .map((element) =>
+      element.map((rgba) => [brightness[rgba.luminosity]].join('')).join('')
+    )
+    .join(colors.bgRed('|\n'));
 
-  console.log(final);
+  console.log(colors.green(final));
 });
