a6da8af9f048a2d8f8470b284515761292b69ea6
Adil Minocherhomjee <adilminocherhomjee@gmail.com>
2020-04-26 12:28:46 -0600

    refactored color schemes

--- ascii-art.js
+++ ascii-art.js
@@ -40,7 +40,7 @@ if (AUTO_PHOTO) {
   );
 }
 
-const picture = Caman('img/selfie.jpg', function () {
+const picture = Caman('img/ascii-pineapple.jpg', function () {
   const SETTINGS = {
     INVERT_COLORS: false,
   };
@@ -103,20 +103,110 @@ const picture = Caman('img/selfie.jpg', function () {
     return brightnessLetter;
   };
   const getColorFromRGBA = (pixel) => {
-    const maxValue = Math.max(pixel.red, pixel.green, pixel.blue);
-    if (maxValue > 200) {
-      return 'grey';
-    } else if (maxValue < 100) {
-      return 'white';
-    } else if (maxValue === pixel.red) {
-      return 'red';
-    } else if (maxValue === pixel.green) {
-      return 'green';
-    } else if (maxValue === pixel.blue) {
-      return 'blue';
-    } else {
-      return 'black';
-    }
+    const red = pixel.red;
+    const green = pixel.green;
+    const blue = pixel.blue;
+    //Ported from:
+    //https://stackoverflow.com/questions/44189508/finding-which-color-is-the-closest-to-the-given-rgb-values-in-c/44463014
+
+    const distinct = [
+      {
+        color: 'black',
+        value: {
+          red: 0,
+          green: 0,
+          blue: 0,
+        },
+      },
+      {
+        color: 'red',
+        value: {
+          red: 255,
+          green: 0,
+          blue: 0,
+        },
+      },
+      {
+        color: 'green',
+        value: {
+          red: 0,
+          green: 190,
+          blue: 0,
+        },
+      },
+      {
+        color: 'yellow',
+        value: {
+          red: 255,
+          green: 235,
+          blue: 0,
+        },
+      },
+      {
+        color: 'blue',
+        value: {
+          red: 67,
+          green: 133,
+          blue: 255,
+        },
+      },
+      {
+        color: 'magenta',
+        value: {
+          red: 255,
+          green: 0,
+          blue: 255,
+        },
+      },
+      {
+        color: 'cyan',
+        value: {
+          red: 100,
+          green: 255,
+          blue: 255,
+        },
+      },
+      {
+        color: 'white',
+        value: {
+          red: 255,
+          green: 255,
+          blue: 255,
+        },
+      },
+      {
+        color: 'grey',
+        value: {
+          red: 128,
+          green: 128,
+          blue: 128,
+        },
+      },
+    ];
+
+    const closestColor = (r, g, b) => {
+      let colorReturn = 'NA';
+      let maxRGB = 255;
+      let biggestDifference = maxRGB * 3 + 1;
+      for (let i = 0; i < distinct.length; i++) {
+        if (
+          Math.sqrt(
+            Math.pow(r - distinct[i].value.red, 2) +
+              Math.pow(g - distinct[i].value.green, 2) +
+              Math.pow(b - distinct[i].value.blue, 2)
+          ) < biggestDifference
+        ) {
+          colorReturn = distinct[i].color;
+          biggestDifference = Math.sqrt(
+            Math.pow(r - distinct[i].value.red, 2) +
+              Math.pow(g - distinct[i].value.green, 2) +
+              Math.pow(b - distinct[i].value.blue, 2)
+          );
+        }
+      }
+      return colorReturn;
+    };
+    return closestColor(red, green, blue);
   };
 
   const final = chunk(
---
+++ colorDifference.js
@@ -0,0 +1,37 @@
+//Ported from:
+//https://stackoverflow.com/questions/44189508/finding-which-color-is-the-closest-to-the-given-rgb-values-in-c/44463014
+
+const distinct = [
+  { color: 'black', value: { red: 0, green: 0, blue: 0 } },
+  { color: 'red', value: { red: 255, green: 0, blue: 0 } },
+  { color: 'green', value: { red: 0, green: 190, blue: 0 } },
+  { color: 'yellow', value: { red: 255, green: 235, blue: 0 } },
+  { color: 'blue', value: { red: 67, green: 133, blue: 255 } },
+  { color: 'magenta', value: { red: 255, green: 0, blue: 255 } },
+  { color: 'cyan', value: { red: 100, green: 255, blue: 255 } },
+  { color: 'white', value: { red: 255, green: 255, blue: 255 } },
+  { color: 'grey', value: { red: 128, green: 128, blue: 128 } },
+];
+
+const closestColor = (r, g, b) => {
+  let colorReturn = 'NA';
+  let maxRGB = 255;
+  let biggestDifference = maxRGB * 3 + 1;
+  for (let i = 0; i < distinct.length; i++) {
+    if (
+      Math.sqrt(
+        Math.pow(r - distinct[i].value.red, 2) +
+          Math.pow(g - distinct[i].value.green, 2) +
+          Math.pow(b - distinct[i].value.blue, 2)
+      ) < biggestDifference
+    ) {
+      colorReturn = distinct[i].color;
+      biggestDifference = Math.sqrt(
+        Math.pow(r - distinct[i].value.red, 2) +
+          Math.pow(g - distinct[i].value.green, 2) +
+          Math.pow(b - distinct[i].value.blue, 2)
+      );
+    }
+  }
+  return colorReturn;
+};
